2087cd18509045755f7e8b2df4503275ea597ba3
ArtKoSolo <kondratov.qa@gmail.com>
2023-05-15 15:40:19 +0300

    Homework 11

--- src/main/java/Homework11/Homework.java
+++ src/main/java/Homework11/Homework.java
@@ -10,6 +10,11 @@ public class Homework {
       sample.put (3, "value 3");
       sample.put (1, "value 1");
       sample.put (2, "value 2");
+      sample.put (8, "value 8");
+      sample.put (6, "value 6");
+      sample.put (5, "value 5");
+      sample.put (4, "value 4");
+      sample.put (7, "value 7");
       for (Map.Entry<Integer, String> l:sample.entrySet ()) {
          System.out.println (l);
       }
